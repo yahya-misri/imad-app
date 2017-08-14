b843259400b537e6485da16124fc4e7aff638dd1
yahya-misri <30749661+yahya-misri@users.noreply.github.com>
2017-08-14 23:50:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+//app.get('/page1',function(req.res) {
+//});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
